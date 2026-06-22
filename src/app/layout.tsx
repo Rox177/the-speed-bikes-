e2bb9cb8daebc6f625057dc126109a55376d4e6e
Taha Ahmed maan <taha.177maan@icloud.com>
2026-06-22 14:10:03 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import { Header } from "@/components/layout/header/header";
 import { Footer } from "@/components/layout/footer/footer";
 import { Providers } from "@/components/shared/providers";
+import { SpeedInsights } from "@vercel/speed-insights/next"; 
+
 
 export const metadata: Metadata = {
   title: "VoltTrail | Premium E-Bike Platform",
